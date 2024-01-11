e3560ad58618571723cdd5ffcc2f5857f5a91422
jhfkf <jhfappkf@163.com>
2024-01-11 11:15:28 +0800

    优化播放声音

--- lib/util/snackbar.dart
+++ lib/util/snackbar.dart
@@ -1,51 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tencent_cloud_chat_uikit/util/toast.dart';
 
 class SnackBarUtils {
 
   static showMsg(String msg) {
-    Get.snackbar(
-      "",
-      "",
-      titleText: const Text(
-        "温馨提示",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          fontSize: 16,
-        ),
-      ),
-      messageText: Text(
-        msg,
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          fontWeight: FontWeight.w300,
-          fontSize: 14,
-        ),
-      ),
-    );
+    ToastUtils.toast(msg);
+    // Get.snackbar(
+    //   "",
+    //   "",
+    //   titleText: const Text(
+    //     "温馨提示",
+    //     textAlign: TextAlign.center,
+    //     style: TextStyle(
+    //       fontWeight: FontWeight.w800,
+    //       fontSize: 16,
+    //     ),
+    //   ),
+    //   messageText: Text(
+    //     msg,
+    //     textAlign: TextAlign.center,
+    //     style: const TextStyle(
+    //       fontWeight: FontWeight.w300,
+    //       fontSize: 14,
+    //     ),
+    //   ),
+    // );
   }
 
   static showNoPrivateChat() {
-    Get.snackbar(
-      "",
-      "",
-      titleText: const Text(
-        "温馨提示",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          fontSize: 16,
-        ),
-      ),
-      messageText: const Text(
-        "该群禁止私聊",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontWeight: FontWeight.w300,
-          fontSize: 14,
-        ),
-      ),
-    );
+    ToastUtils.toast("该群禁止私聊");
+    // Get.snackbar(
+    //   "",
+    //   "",
+    //   titleText: const Text(
+    //     "温馨提示",
+    //     textAlign: TextAlign.center,
+    //     style: TextStyle(
+    //       fontWeight: FontWeight.w800,
+    //       fontSize: 16,
+    //     ),
+    //   ),
+    //   messageText: const Text(
+    //     "该群禁止私聊",
+    //     textAlign: TextAlign.center,
+    //     style: TextStyle(
+    //       fontWeight: FontWeight.w300,
+    //       fontSize: 14,
+    //     ),
+    //   ),
+    // );
   }
 }
